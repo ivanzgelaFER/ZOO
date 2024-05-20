af43bc8cf13b12375282406679f60f1f1619d000
ivanzgelaFER <ivanzgelagit@gmail.com>
2024-05-20 22:58:59 +0200

    Integration test

---
+++ ZOO_Management.IntegrationTest/SektoriServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ZOO_Management.IntegrationTest
+{
+    public class SektoriServiceTests
+    {
+
+    }
+}
